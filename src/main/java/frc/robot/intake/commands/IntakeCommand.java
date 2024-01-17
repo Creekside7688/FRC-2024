2ec1c96bca28195bf953a6a5464db95c7fdf7270
Zylve <70604613+Zylve@users.noreply.github.com>
2024-01-16 20:33:12 -0800

    setup subsystems

---
+++ src/main/java/frc/robot/intake/commands/IntakeCommand.java
@@ -0,0 +1,26 @@
+package frc.robot.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.intake.Intake;
+
+public class IntakeCommand extends Command {
+    public IntakeCommand(Intake intake) {
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
