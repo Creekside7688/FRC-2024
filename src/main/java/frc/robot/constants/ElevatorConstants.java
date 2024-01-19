e61a6440de2517f983096b67e1c5daa754eaa5db
Zylve <70604613+Zylve@users.noreply.github.com>
2024-01-18 21:58:50 -0800

    Move controller onboard

---
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -0,0 +1,29 @@
+package frc.robot.constants;
+
+import com.revrobotics.CANSparkBase.IdleMode;
+
+public class ElevatorConstants {
+
+    public static final int MOTOR_ID = 9;
+    public static final IdleMode IDLE_MODE = IdleMode.kBrake;
+    public static final int CURRENT_LIMIT = 20;
+
+    public static final boolean ENCODER_INVERTED = false;
+    public static final double ENCODER_POSITION_FACTOR = 1.0;
+    public static final double ENCODER_VELOCITY_FACTOR = 1.0 / 60.0;
+
+    public static final double P = 0.04;
+    public static final double I = 0.0;
+    public static final double D = 0.0;
+    public static final double PID_MINIMUM_OUTPUT = -1.0;
+    public static final double PID_MAXIMUM_OUTPUT = 1.0;
+
+    public static final double S = 0.0;
+    public static final double G = 0.0;
+    public static final double V = 0.0;
+    public static final double A = 0.0;
+
+    public static final double MAX_VELOCITY = 1.0;
+    public static final double MAX_ACCELERATION = 1.0;
+
+}
--- src/main/java/frc/robot/elevator/Elevator.java
+++ src/main/java/frc/robot/elevator/Elevator.java
@@ -1,30 +1,64 @@
 package frc.robot.elevator;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ElevatorConstants;
 
 public class Elevator extends SubsystemBase {
-    private final CANSparkMax motor = new CANSparkMax(9, MotorType.kBrushless);
+    private final CANSparkMax motor;
+    private final RelativeEncoder encoder;
+
+    private final Constraints constraints;
+    private TrapezoidProfile.State setpoint;
+    private TrapezoidProfile.State goal;
+
+    private final SparkPIDController feedbackController;
+    private final ElevatorFeedforward feedforwardController;
+
     private final Timer timer = new Timer();
-
-    private final Constraints constraints = new Constraints(1.0, 1.0);
-    private TrapezoidProfile.State setpoint = new TrapezoidProfile.State(0.0, 0.0);
-    private TrapezoidProfile.State goal = new TrapezoidProfile.State(0.0, 0.0);
-
-    private double lastVelocity = 0.0;
-
-    private final PIDController feedbackController = new PIDController(0.0, 0.0, 0.0);
-    private final ElevatorFeedforward feedforwardController = new ElevatorFeedforward(1, 0.1, 0.1, 0.1);
+    private double lastVelocity;
 
     public Elevator() {
+        motor = new CANSparkMax(ElevatorConstants.MOTOR_ID, MotorType.kBrushless);
+        motor.restoreFactoryDefaults();
+        motor.setIdleMode(ElevatorConstants.IDLE_MODE);
+        motor.setSmartCurrentLimit(ElevatorConstants.CURRENT_LIMIT);
+        motor.burnFlash();
+
+        encoder = motor.getEncoder();
+        encoder.setPositionConversionFactor(ElevatorConstants.ENCODER_POSITION_FACTOR);
+        encoder.setVelocityConversionFactor(ElevatorConstants.ENCODER_VELOCITY_FACTOR);
+        encoder.setInverted(ElevatorConstants.ENCODER_INVERTED);
+        encoder.setPosition(0);
+
+        feedbackController = motor.getPIDController();
+        feedbackController.setFeedbackDevice(encoder);
+        feedbackController.setP(ElevatorConstants.P);
+        feedbackController.setI(ElevatorConstants.I);
+        feedbackController.setD(ElevatorConstants.D);
+        feedbackController.setFF(0);
+        feedbackController.setOutputRange(ElevatorConstants.PID_MINIMUM_OUTPUT, ElevatorConstants.PID_MAXIMUM_OUTPUT);
+
+        feedforwardController = new ElevatorFeedforward(
+            ElevatorConstants.S,
+            ElevatorConstants.G,
+            ElevatorConstants.V,
+            ElevatorConstants.A);
+
+        constraints = new Constraints(ElevatorConstants.MAX_VELOCITY, ElevatorConstants.MAX_ACCELERATION);
+        setpoint = new TrapezoidProfile.State(0, 0);
+        goal = new TrapezoidProfile.State(0, 0);
+        lastVelocity = 0;
     }
 
     @Override
@@ -34,13 +68,16 @@ public class Elevator extends SubsystemBase {
 
         TrapezoidProfile profile = new TrapezoidProfile(constraints);
         setpoint = profile.calculate(deltaTime, setpoint, goal);
-
-        double feedback = feedbackController.calculate(0, 0);
+        
         double feedforward = feedforwardController.calculate(setpoint.velocity, (setpoint.velocity - lastVelocity) / deltaTime);
+        
+        feedbackController.setReference(
+            setpoint.position,
+            CANSparkBase.ControlType.kPosition,
+            0,
+            feedforward);
 
         lastVelocity = setpoint.velocity;
-
-        motor.setVoltage(feedback + feedforward);
     }
 
     public TrapezoidProfile.State getGoal() {
