412b9548f698820e1a33c9222a0ba8078e4b96c1
Zylve <70604613+Zylve@users.noreply.github.com>
2024-01-21 21:46:18 -0800

    Elevator documentation

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -6,7 +6,7 @@ public class ElevatorConstants {
 
     public static final int MOTOR_ID = 9;
     public static final IdleMode IDLE_MODE = IdleMode.kBrake;
-    public static final int CURRENT_LIMIT = 20;
+    public static final int CURRENT_LIMIT = 50;
 
     public static final boolean ENCODER_INVERTED = false;
     public static final double ENCODER_POSITION_FACTOR = 1.0;
@@ -26,4 +26,6 @@ public class ElevatorConstants {
     public static final double MAX_VELOCITY = 1.0;
     public static final double MAX_ACCELERATION = 1.0;
 
+    public static final double MAX_HEIGHT = 28.0;
+    public static final double TOLERANCE = 0.1;
 }
--- src/main/java/frc/robot/elevator/Elevator.java
+++ src/main/java/frc/robot/elevator/Elevator.java
@@ -28,6 +28,9 @@ public class Elevator extends SubsystemBase {
     private final Timer timer = new Timer();
     private double lastVelocity;
 
+    /**
+     * Creates an elevator subsystem. To change settings, use the ElevatorConstants class.
+     */
     public Elevator() {
         motor = new CANSparkMax(ElevatorConstants.MOTOR_ID, MotorType.kBrushless);
         motor.restoreFactoryDefaults();
@@ -68,9 +71,9 @@ public class Elevator extends SubsystemBase {
 
         TrapezoidProfile profile = new TrapezoidProfile(constraints);
         setpoint = profile.calculate(deltaTime, setpoint, goal);
-        
+
         double feedforward = feedforwardController.calculate(setpoint.velocity, (setpoint.velocity - lastVelocity) / deltaTime);
-        
+
         feedbackController.setReference(
             setpoint.position,
             CANSparkBase.ControlType.kPosition,
@@ -80,15 +83,50 @@ public class Elevator extends SubsystemBase {
         lastVelocity = setpoint.velocity;
     }
 
+    /**
+     * Gets the current <STRONG>goal</STRONG> of the elevator.
+     * 
+     * @return The current goal of the elevator in metres and metres per second.
+     */
     public TrapezoidProfile.State getGoal() {
         return this.goal;
     }
 
+    /**
+     * Sets the <STRONG>goal</STRONG> of the elevator.
+     * 
+     * @param goal The goal position and velocity of the elevator in metres and metres per second.
+     */
     public void setGoal(TrapezoidProfile.State goal) {
         this.goal = goal;
     }
 
+    /**
+     * Sets the goal height of the elevator.
+     * 
+     * @param height The goal position of the elevator in metres.
+     */
     public void setHeight(double height) {
         this.goal = new State(height, 0);
+    }
+
+    /**
+     * Gets whether the elevator is at its goal within the specified tolerance in the constants file. Use this to check when to move on to the next command in a sequential command
+     * group.
+     * 
+     * @return Whether the elevator is at its goal within the specified tolerance.
+     */
+    public boolean atGoal() {
+        return Math.abs(goal.position - encoder.getPosition()) < ElevatorConstants.TOLERANCE;
+    }
+
+    /**
+     * Gets whether the elevator is at its goal within the passed tolerance. Use this to check when to move on to the next command in a sequential command group.
+     * 
+     * @param tolerance The tolerance in metres.
+     * @return Whether the elevator is at its goal within the specified tolerance.
+     */
+    public boolean atGoal(double tolerance) {
+        return Math.abs(goal.position - encoder.getPosition()) < tolerance;
     }
 }
