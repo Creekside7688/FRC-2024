ce4cdce5b10b91bd6fff8b3e939ee8b1f63a1185
MProne <bren_tom_wong@shaw.ca>
2024-01-26 14:50:49 -0800

    Added Jeremy's command and subsystem

--- src/main/java/frc/robot/shooter/Shooter.java
+++ src/main/java/frc/robot/shooter/Shooter.java
@@ -1,12 +1,27 @@
 package frc.robot.shooter;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
-public class Shooter extends SubsystemBase {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ShooterConstants;;;
+
+public class Shooter extends SubsystemBase { //similar to making a scratch sprite
+    private final CANSparkMax shooterMotor = new CANSparkMax(ShooterConstants.SHOOTER_MOTOR_ID, MotorType.kBrushless);;
+
     public Shooter() {
+       
     }
 
     @Override
     public void periodic() {
+    }
+
+    public void shooterMotorOn() {
+        shooterMotor.set(ShooterConstants.SHOOTER_SPEED);
+    }
+    
+    public void shooterMotorOff() {
+        shooterMotor.set(ShooterConstants.SHOOTER_STOP);
     }
 }
