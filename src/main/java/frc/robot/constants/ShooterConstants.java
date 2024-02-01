3e166e03dbce7cccce4a3013fd783ddbfa8ac5dc
JefferyHuman <althuman_@outlook.com>
2024-01-31 16:07:04 -0800

    Update Shooter Code

---
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.constants;
+
+public class ShooterConstants {
+    public static final int DELAY = 1;
+    public static final double SHOOTER_OFF = 0;
+    public static final double SHOOTER_ON = 1;
+}
--- src/main/java/frc/robot/shooter/Shooter.java
+++ src/main/java/frc/robot/shooter/Shooter.java
@@ -1,27 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.shooter;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.ShooterConstants;;;
 
-public class Shooter extends SubsystemBase { //similar to making a scratch sprite
-    private final CANSparkMax shooterMotor = new CANSparkMax(ShooterConstants.SHOOTER_MOTOR_ID, MotorType.kBrushless);;
+public class Shooter extends SubsystemBase {
+  /** Creates a new Shooter. */
+  public Shooter() {}
 
-    public Shooter() {
-       
-    }
-
-    @Override
-    public void periodic() {
-    }
-
-    public void shooterMotorOn() {
-        shooterMotor.set(ShooterConstants.SHOOTER_SPEED);
-    }
-    
-    public void shooterMotorOff() {
-        shooterMotor.set(ShooterConstants.SHOOTER_STOP);
-    }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
