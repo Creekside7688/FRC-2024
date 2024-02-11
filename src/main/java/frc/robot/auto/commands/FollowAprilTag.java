1f679fad65bf3625d1a74c3f756505d3ba3925e9
Vuk Tacic <70604613+Zylve@users.noreply.github.com>
2024-02-11 13:19:10 -0800

    Document FollowAprilTag.java

--- src/main/java/frc/robot/auto/commands/FollowAprilTag.java
+++ src/main/java/frc/robot/auto/commands/FollowAprilTag.java
@@ -25,7 +25,7 @@ public class FollowAprilTag extends Command {
     private final PhotonCamera camera;
     private final Supplier<Pose2d> poseProvider;
 
-    private PhotonTrackedTarget lastTarget;
+    private PhotonTrackedTarget lastTag;
 
     private static final TrapezoidProfile.Constraints X_CONSTRAINTS = new TrapezoidProfile.Constraints(3, 2);
     private static final TrapezoidProfile.Constraints Y_CONSTRAINTS = new TrapezoidProfile.Constraints(3, 2);
@@ -36,9 +36,12 @@ public class FollowAprilTag extends Command {
     private final ProfiledPIDController tController = new ProfiledPIDController(2, 0, 0, T_CONSTRAINTS);
 
     private static final int TAG_ID = 6;
-    private static final Transform3d DISTANCE_FROM_TAG = new Transform3d(
+
+    // How far we **want** to be from the tag
+    private static final Transform3d DESIRED_DISTANCE = new Transform3d(
         new Translation3d(1.5, 0.0, 0.0),
-        new Rotation3d(0.0, 0.0, Math.PI));
+        new Rotation3d(0.0, 0.0, Math.PI)
+    );
 
     public FollowAprilTag(SwerveDrive swerveDrive, PhotonCamera camera, Supplier<Pose2d> poseProvider) {
         this.swerveDrive = swerveDrive;
@@ -55,10 +58,13 @@ public class FollowAprilTag extends Command {
 
     @Override
     public void initialize() {
-        lastTarget = null;
+        // Clear targets from previous run
+        lastTag = null;
 
+        // Get current pose of the robot on the field.
         Pose2d robotPose = poseProvider.get();
 
+        // Reset the PID controllers to the current robot pose.
         xController.reset(robotPose.getX());
         yController.reset(robotPose.getY());
         tController.reset(robotPose.getRotation().getRadians());
@@ -68,66 +74,85 @@ public class FollowAprilTag extends Command {
     public void execute() {
         Pose2d robotPose2d = poseProvider.get();
 
-        Pose3d robotPose = new Pose3d(
+        // Convert the 2d robot pose to a 3d one.
+        Pose3d robotPose3d = new Pose3d(
             robotPose2d.getX(),
             robotPose2d.getY(),
             0.0,
-            new Rotation3d(0.0, 0.0, robotPose2d.getRotation().getRadians()));
+            new Rotation3d(0.0, 0.0, robotPose2d.getRotation().getRadians())
+        );
 
-        PhotonPipelineResult photonRes = camera.getLatestResult();
+        // Get the latest results from our limelight.
+        PhotonPipelineResult photonResults = camera.getLatestResult();
 
-        if(photonRes.hasTargets()) {
+        // If we see tags
+        if(photonResults.hasTargets()) {
 
-            // Find the tag we want to chase
-            Optional<PhotonTrackedTarget> targetOpt = photonRes.getTargets().stream()
-                .filter(t -> t.getFiducialId() == TAG_ID)
-                .filter(t -> !t.equals(lastTarget) && t.getPoseAmbiguity() <= .2 && t.getPoseAmbiguity() != -1)
+            // Filter targets
+            Optional<PhotonTrackedTarget> filteredTag = photonResults.getTargets().stream()
+                .filter(t -> t.getFiducialId() == TAG_ID) // To find the tag ID we want
+                .filter(t -> !t.equals(lastTag)) // That is not in **exactly** the same position as the last one (To prevent recalculations)
+                .filter(t -> t.getPoseAmbiguity() <= .2) // That is not too ambiguous
+                .filter(t -> t.getPoseAmbiguity() != -1) // Where something didn't go wrong
                 .findFirst();
 
-            if(targetOpt.isPresent()) {
-                PhotonTrackedTarget target = targetOpt.get();
-                // This is new target data, so recalculate the goal
-                lastTarget = target;
+            // If such a tag exists
+            if(filteredTag.isPresent()) {
+                // Get the target information
+                PhotonTrackedTarget tag = filteredTag.get();
 
-                // Transform the robot's pose to find the camera's pose
-                Pose3d cameraPose = robotPose.transformBy(APRILTAG_CAMERA_TO_ROBOT);
+                // Store tag information to compare in the next period
+                lastTag = tag;
 
-                // Trasnform the camera's pose to the target's pose
-                Transform3d camToTarget = target.getBestCameraToTarget();
-                Pose3d targetPose = cameraPose.transformBy(camToTarget);
+                // Transform the robot pose by the camera offset to find the exact position of the camera in the field. (Adding the two poses together)
+                Pose3d cameraPose = robotPose3d.transformBy(APRILTAG_CAMERA_TO_ROBOT);
 
-                // Transform the tag's pose to set our goal
-                Pose2d goalPose = targetPose.transformBy(DISTANCE_FROM_TAG).toPose2d();
+                // Get the position of the camera relative to the tag
+                Transform3d cameraToTag = tag.getBestCameraToTarget();
 
-                // Drive
+                // Transform the camera pose by the distance to the tag to get the exact position of the tag in the field.
+                Pose3d tagPose = cameraPose.transformBy(cameraToTag);
+
+                // Transform the tag pose by the desired distance to get the pose we want to drive to.
+                Pose2d goalPose = tagPose.transformBy(DESIRED_DISTANCE).toPose2d();
+
+                // Set the PID controller goals.
                 xController.setGoal(goalPose.getX());
                 yController.setGoal(goalPose.getY());
                 tController.setGoal(goalPose.getRotation().getRadians());
             }
         }
 
-        if(lastTarget == null) {
-            // No target has been visible
+        // If we don't see any tags
+        if(lastTag == null) {
+            // Lock the drive position
             swerveDrive.lockPosition();
+
+            // Otherwise,
         } else {
-            // Drive to the target
-            double xSpeed = xController.calculate(robotPose.getX());
+            // Drive the robot towards the goal.
+            double xSpeed = xController.calculate(robotPose3d.getX());
+
             if(xController.atGoal()) {
                 xSpeed = 0;
             }
 
-            double ySpeed = yController.calculate(robotPose.getY());
+            double ySpeed = yController.calculate(robotPose3d.getY());
+
             if(yController.atGoal()) {
                 ySpeed = 0;
             }
 
             double tSpeed = tController.calculate(robotPose2d.getRotation().getRadians());
+
             if(tController.atGoal()) {
                 tSpeed = 0;
             }
 
+            // Drive the robot using the calculated speeds
             swerveDrive.driveRelative(
-                ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, tSpeed, robotPose2d.getRotation()));
+                ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, tSpeed, robotPose2d.getRotation())
+            );
         }
     }
 
