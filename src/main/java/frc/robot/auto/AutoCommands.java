187a29f81cb684ef2a69165ffc55508e2e502f63
Vuk Tacic <70604613+Zylve@users.noreply.github.com>
2024-02-16 13:58:34 -0800

    Cleanup autocommands

--- src/main/java/frc/robot/auto/AutoCommands.java
+++ src/main/java/frc/robot/auto/AutoCommands.java
@@ -5,8 +5,6 @@
 package frc.robot.auto;
 
 import org.photonvision.PhotonCamera;
-
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.auto.commands.AmpAlign;
