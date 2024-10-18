61319316adbc153422cb23c5d923a3e4edd6db11
Imeanbusiness <dominic.adam.koh@gmail.com>
2024-10-17 19:20:25 -0700

    Created Limelight.java.

---
+++ src/main/java/frc/robot/limelight/Limelight.java
@@ -0,0 +1,20 @@
+package frc.robot.limelight;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.*;
+
+public class Limelight extends SubsystemBase {
+
+
+    public Limelight() {
+        
+    }
+
+    @Override
+    public void periodic() {
+    }
+
+}
