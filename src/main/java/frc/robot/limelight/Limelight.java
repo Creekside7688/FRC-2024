97f62f3d63456f50a3016f21fb8ea6337ccf4bdd
Imeanbusiness <dominic.adam.koh@gmail.com>
2024-11-09 17:37:43 -0800

    Made methods for Limelight
    Hahahahahahahaha

--- src/main/java/frc/robot/limelight/Limelight.java
+++ src/main/java/frc/robot/limelight/Limelight.java
@@ -4,13 +4,43 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import org.photonvision.*;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class Limelight extends SubsystemBase {
+    private final PhotonCamera robotcamera = new PhotonCamera("photonvision"); 
+
+
 
 
     public Limelight() {
         
+    }
+    
+
+    public double TargetYaw() {
+        var results = robotcamera.getLatestResult();
+        PhotonTrackedTarget target = results.getBestTarget();
+        return target.getYaw();
+    }
+
+    public double TargetPitch() {
+        var results = robotcamera.getLatestResult();
+        PhotonTrackedTarget target = results.getBestTarget();
+        return target.getPitch();
+    }
+
+    public int getAprilTag() {
+        var results = robotcamera.getLatestResult();
+        PhotonTrackedTarget target = results.getBestTarget();
+        return target.getFiducialId();
+    }
+
+    public boolean CameraHasTargets() {
+        var results = robotcamera.getLatestResult();
+        return results.hasTargets();
     }
 
     @Override
